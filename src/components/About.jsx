0bafc02e96ae092bfd6aa0c84aa44ddb65f5fca7
Abdulrahman <rachmantech@gmail.com>
2024-12-10 23:17:29 +0200

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+       My name is Raja Aifa, I am a fucking professional software developer and I have many projects, but I will not show them to you because of envy. If you need anything, do not contact me. I am asleep. If you are going to pay more than $5,000 and laplaplaa laplaplaa laplaplaa laplaplaa laplaplaa  and bye 
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,285 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  starbucks,
+  omnifood,
+  meta,
+  refine,
+  clone,
+  dalle,
+  tesla,
+  shopify,
+  threejs,
+  omnifoodLogo,
+  openai,
+  three,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact Me",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Frontend Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  // {
+  //   name: "docker",
+  //   icon: docker,
+  // },
+];
+
+const experiences = [
+  {
+    title: "Frontend Developer",
+    company_name: "Omnifood",
+    icon: omnifoodLogo,
+    iconBg: "#FDF2EA",
+    date: "Feb 2023 - March 2023",
+    points: [
+      "Eat",
+      "drink",
+      "code",
+      "sleep",
+    ],
+  },
+  {
+    title: "React Developer",
+    company_name: "Codex ChatGPI Clone",
+    icon: openai,
+    iconBg: "#fff",
+    date: "Jan 2023 - Feb 2023",
+    points: [
+      "Eat",
+      "drink",
+      "code",
+      "sleep",
+    ],
+  },
+  {
+    title: "Web Developer Portfolio",
+    company_name: "3D React Charts",
+    icon: threejs,
+    iconBg: "#fff",
+    date: "Jan 2023 - Jan 2023",
+    points: [
+      "Eat",
+      "drink",
+      "code",
+      "sleep",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "wow Raja is a very fucking good programmer she did my shit very professionally i recommend her .",
+    name: "bitch",
+    designation: "CFO",
+    company: "Starbucks",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "wow Raja is a very fucking good programmer she did my shit very professionally i recommend her .",
+    name: "fucker",
+    designation: "COO",
+    company: "Omnifood",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "wow Raja is a very fucking good programmer she did my shit very professionally i recommend her ",
+    name: "fucker",
+    designation: "CTO",
+    company: "Real Estates",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Project 1",
+    description:
+      "lap lap lap laplalap lap lap laplalap lap lap laplalap lap lap laplalap lap lap laplalap lap lap laplalap lap lap lapla.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "refine",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: refine,
+    source_code_link: "https://github.com/raja1100011",
+    live_link: "https://img.freepik.com/free-vector/fuck-you-symbol-comic-style_23-2148684350.jpg?t=st=1733864906~exp=1733868506~hmac=93d613a78ac182a31c8e213852aee2b76b476533f7f7d7ab1a1535d7f0a75454&w=740",
+    youtube_link: "https://img.freepik.com/free-vector/fuck-you-symbol-comic-style_23-2148684350.jpg?t=st=1733864906~exp=1733868506~hmac=93d613a78ac182a31c8e213852aee2b76b476533f7f7d7ab1a1535d7f0a75454&w=740",
+  },
+  {
+    name: "Project 2",
+    description:
+      "lap lap lap laplalap lap lap laplalap lap lap laplalap lap lap laplalap lap lap laplalap lap lap laplalap lap lap lapla.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "openai-api",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: dalle,
+    source_code_link: "https://github.com/raja1100011",
+    live_link: "https://img.freepik.com/free-vector/fuck-you-symbol-comic-style_23-2148684350.jpg?t=st=1733864906~exp=1733868506~hmac=93d613a78ac182a31c8e213852aee2b76b476533f7f7d7ab1a1535d7f0a75454&w=740",
+    youtube_link: "https://img.freepik.com/free-vector/fuck-you-symbol-comic-style_23-2148684350.jpg?t=st=1733864906~exp=1733868506~hmac=93d613a78ac182a31c8e213852aee2b76b476533f7f7d7ab1a1535d7f0a75454&w=740",
+  },
+  {
+    name: "Project 3",
+    description:
+      "lap lap lap laplalap lap lap laplalap lap lap laplalap lap lap laplalap lap lap laplalap lap lap laplalap lap lap lapla.",
+    tags: [
+      {
+        name: "html",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "js",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: omnifood,
+    source_code_link:
+      "https://github.com/raja1100011",
+    live_link: "https://img.freepik.com/free-vector/fuck-you-symbol-comic-style_23-2148684350.jpg?t=st=1733864906~exp=1733868506~hmac=93d613a78ac182a31c8e213852aee2b76b476533f7f7d7ab1a1535d7f0a75454&w=740",
+    youtube_link: "https://img.freepik.com/free-vector/fuck-you-symbol-comic-style_23-2148684350.jpg?t=st=1733864906~exp=1733868506~hmac=93d613a78ac182a31c8e213852aee2b76b476533f7f7d7ab1a1535d7f0a75454&w=740",
+  },
+  // {
+  //   name: "Starbucks Clone",
+  //   description: "A clone of the Starbucks website built using HTML5 & CSS3.",
+  //   tags: [
+  //     {
+  //       name: "html",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "js",
+  //       color: "green-text-gradient",
+  //     },
+  //     {
+  //       name: "css",
+  //       color: "pink-text-gradient",
+  //     },
+  //   ],
+  //   image: clone,
+  //   source_code_link: "https://github.com/JagadeeshKJ/Starbucks-Clone",
+  //   live_link: "https://starbucks-clone-ashy.vercel.app/",
+  //   youtube_link: "https://www.youtube.com/@jagadeeshkoyya",
+  // },
+];
+
+export { services, technologies, experiences, testimonials, projects };
